934739bf89f62611da196eb493f9f4345a4149b4
Firith Glaurung <firith.glaurung@gmail.com>
2018-08-01 16:13:57 +0200

    SimpleLinearRegression: Import data and preprocess it
    Will the rest of the code as needed.

--- SimpleLinearRegression/simple_linear_regression.py
+++ SimpleLinearRegression/simple_linear_regression.py
@@ -1,47 +1,43 @@
 # Simple Linear Regression
 
 # Importing the libraries
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas
 
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+sys.path.insert(0, "../DataPreprocessing")
+from preprocess import Preprocess
+preprocess = Preprocess()
+preprocess.doPreprocess("Salary_Data.csv", -1, 1)
+dataset = preprocess.dataset
+dependant_matrix = pandas.DataFrame(preprocess.dependent_matrix)
+dependant_train_matrix = pandas.DataFrame(preprocess.dependent_train_matrix)
+dependant_test_matrix = pandas.DataFrame(preprocess.dependent_test_matrix)
+independant_matrix = pandas.DataFrame(preprocess.independent_matrix)
+independant_train_matrix = pandas.DataFrame(preprocess.independent_train_matrix)
+independant_test_matrix = pandas.DataFrame(preprocess.independent_test_matrix)
 
 # Fitting Simple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+#from sklearn.linear_model import LinearRegression
+#regressor = LinearRegression()
+#regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+#y_pred = regressor.predict(X_test)
 
 # Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+#plt.scatter(X_train, y_train, color = 'red')
+#plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+#plt.title('Salary vs Experience (Training set)')
+#plt.xlabel('Years of Experience')
+#plt.ylabel('Salary')
+#plt.show()
 
 # Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+#plt.scatter(X_test, y_test, color = 'red')
+#plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+#plt.title('Salary vs Experience (Test set)')
+#plt.xlabel('Years of Experience')
+#plt.ylabel('Salary')
+#plt.show()
