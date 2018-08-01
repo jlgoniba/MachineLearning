42906b630e728648ac0147f97e33186bb714f02f
Firith Glaurung <firith.glaurung@gmail.com>
2018-08-01 16:33:17 +0200

    SimpleLinearRegression: Uncomment prediction code
    Still wip. It only refactors the code from the course to the one
    found in the preprocess class

--- SimpleLinearRegression/simple_linear_regression.py
+++ SimpleLinearRegression/simple_linear_regression.py
@@ -19,25 +19,25 @@ independant_train_matrix = pandas.DataFrame(preprocess.independent_train_matrix)
 independant_test_matrix = pandas.DataFrame(preprocess.independent_test_matrix)
 
 # Fitting Simple Linear Regression to the Training set
-#from sklearn.linear_model import LinearRegression
-#regressor = LinearRegression()
-#regressor.fit(X_train, y_train)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(preprocess.independent_train_matrix, preprocess.dependent_train_matrix)
 
 # Predicting the Test set results
-#y_pred = regressor.predict(X_test)
+dependant_prediction = regressor.predict(preprocess.independent_test_matrix)
 
 # Visualising the Training set results
-#plt.scatter(X_train, y_train, color = 'red')
-#plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-#plt.title('Salary vs Experience (Training set)')
-#plt.xlabel('Years of Experience')
-#plt.ylabel('Salary')
-#plt.show()
+plt.scatter(preprocess.independent_train_matrix, preprocess.dependent_train_matrix, color = 'red')
+plt.plot(preprocess.independent_train_matrix, regressor.predict(preprocess.independent_train_matrix), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
 # Visualising the Test set results
-#plt.scatter(X_test, y_test, color = 'red')
-#plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-#plt.title('Salary vs Experience (Test set)')
-#plt.xlabel('Years of Experience')
-#plt.ylabel('Salary')
-#plt.show()
+plt.scatter(preprocess.independent_test_matrix, preprocess.dependent_test_matrix, color = 'red')
+plt.plot(preprocess.independent_train_matrix, regressor.predict(preprocess.independent_train_matrix), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
